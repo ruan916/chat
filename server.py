fc2f7119f7c1ed89dec6f5bd8bcd56fe927c794c
Ruan Themoteo Nunes <ruantmtnunes@gmail.com>
2021-05-06 16:58:56 -0300

    Create server.py

---
+++ server.py
@@ -0,0 +1,51 @@
+import socket, threading
+
+HOST = '192.168.0.110'
+PORT = 12000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST,PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(2048)
+            print(f"{nicknames[clients.index(client)]} says {message}")
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames(index)
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}!")
+
+        client.send("NICK".encode('utf8'))
+        nickname = client.recv(2048)
+
+        nicknames.append(nickname)
+        clients.append(client)
+
+        print(f"Nickname of the client is {nickname}")
+        broadcast(f"{nickname} connected to the server!\n".encode('utf8'))
+        client.send("Connected to the server".encode('utf8'))
+
+        thread = threading.Thread(target=handle,args=(client,))
+        thread.start()
+    
+print("Server running...")
+receive()
